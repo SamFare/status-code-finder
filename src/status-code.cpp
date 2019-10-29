4eab599611b429e1d8cb87c6c347944e65f523e6
Samuel Fare <samuel.fare@gmail.com>
2019-10-29 17:17:04 +0000

    :sparkles: Added basic code outline

--- src/status-code.cpp
+++ src/status-code.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include <map>
+#include <string>
 
-int main(int argc, char** argv) { 
-    std::cout << "hello world" << std::endl;
+std::map<int, std::string> statusCodes = {
+        {404, "Not Found"}
+    };
+
+int main(int argc, char* argv[]) { 
+    std::cout << statusCodes.at(404) << std::endl;
+    
 }
