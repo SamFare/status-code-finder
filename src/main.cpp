03760c84e8b1c9cd547f7f6c4827cfa2e45c1278
Sam Fare <samuel.fare@gmail.com>
2019-11-04 22:40:01 +0000

    :sparkles: added pretty print to main cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,12 @@
 #include <string>
 
 #include "status-code-finder/status-code-finder.hpp"
+#include "status-code-pretty-print/status-code-pretty-print.hpp"
 
 
 
 int main(int argc, char* argv[]) {
     auto finder = new StatusCodeFinder(); 
-    std::cout << finder->get(atoi(argv[1]))->getStatusText() << std::endl;
+    auto prettyPrint = new StatusCodePrettyPrint();
+    std::cout << prettyPrint->print(finder->get(atoi(argv[1]))) << std::endl;
 }
